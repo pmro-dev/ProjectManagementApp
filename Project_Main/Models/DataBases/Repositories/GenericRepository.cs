e2cc531057bf7d815e7f3e32f875f8060e22f08e
= <=>
2023-03-25 21:45:13 +0100

    feat Generic Repository

---
+++ Project_Main/Models/DataBases/Repositories/GenericRepository.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace Project_Main.Models.DataBases.Repositories
+{
+	public class GenericRepository<TEntity> : IRepository<TEntity> where TEntity : class
+	{
+		private readonly DbContext _dbContext;
+		private readonly DbSet<TEntity> _dbSet;
+
+		public GenericRepository(DbContext dbContext)
+		{
+			_dbContext = dbContext;
+			_dbSet = _dbContext.Set<TEntity>();
+		}
+
+		public async Task Add(TEntity entity)
+		{
+			await _dbSet.AddAsync(entity);
+		}
+
+		public async Task<IEnumerable<TEntity>> GetAllAsync()
+		{
+			return await _dbSet.ToListAsync();
+		}
+
+		public async Task<TEntity?> GetAsync(object id)
+		{
+			return await _dbSet.FindAsync(id);
+		}
+
+		public async Task<IEnumerable<TEntity>> GetByFilterAsync(Expression<Func<TEntity, bool>>? filter = null)
+		{
+			IQueryable<TEntity> entities = _dbSet;
+
+			if (filter != null)
+			{
+				entities = entities.Where(filter);
+			}
+
+			return await entities.ToListAsync();
+		}
+
+		public void Remove(TEntity entity)
+		{
+			_dbSet.Remove(entity);
+		}
+
+		public void Update(TEntity entity)
+		{
+			_dbSet.Update(entity);
+		}
+	}
+}
