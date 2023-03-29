c6ff8e6a9064dd43b7252ec6ec7f56b4641929ff
= <=>
2023-03-29 23:22:57 +0200

    fix id param type

--- Project_Main/Models/DataBases/Repositories/IUserRepository.cs
+++ Project_Main/Models/DataBases/Repositories/IUserRepository.cs
@@ -4,7 +4,7 @@ namespace Project_Main.Models.DataBases.Repositories
 {
 	public interface IUserRepository : IGenericRepository<UserModel>
 	{
-		Task<UserModel> GetWithDetailsAsync(int id);
+		Task<UserModel> GetWithDetailsAsync(string id);
 		Task<UserModel> GetByNameAndPasswordAsync(string name, string password);
 		Task<bool> IsNameTakenAsync(string name);
 	}
