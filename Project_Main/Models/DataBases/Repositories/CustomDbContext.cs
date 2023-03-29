b806cd08786dba9fa288f5aa42b48211b931ba9e
= <=>
2023-03-29 18:46:42 +0200

    feat Custom DbContext with overriden OnModelCreating method that set basic settings for TaskModel and TaskTagModel Entities, and set conversion of TaskStatusType to and from Db to a specific types.

---
+++ Project_Main/Models/DataBases/Repositories/CustomDbContext.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using Project_DomainEntities;
+using Project_Main.Infrastructure.Helpers;
+using static Project_DomainEntities.Helpers.TaskStatusHelper;
+
+namespace Project_Main.Models.DataBases.Repositories
+{
+	public class CustomDbContext : DbContext
+	{
+		private readonly ILogger<CustomDbContext> _logger;
+
+		/// <summary>
+		/// Initilizes object and Ensures that database is created.
+		/// </summary>
+		public CustomDbContext(DbContextOptions<CustomDbContext> options, ILogger<CustomDbContext> logger) : base(options)
+		{
+			_logger = logger;
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			base.OnModelCreating(modelBuilder);
+
+			var todoItemBuilder = modelBuilder.Entity<TaskModel>();
+
+			todoItemBuilder.Property(x => x.Status)
+				.HasConversion(new EnumToStringConverter<TaskStatusType>());
+
+			modelBuilder.Entity<TaskTagModel>().HasKey(tt => new { tt.TaskId, tt.TagId });
+
+			_logger.LogInformation(Messages.BuildingSucceedLogger, nameof(OnModelCreating), nameof(CustomDbContext));
+		}
+	}
+}
