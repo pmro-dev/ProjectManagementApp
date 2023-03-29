b8f4b86b373f865b43ea5d97a749dc0cbc330073
= <=>
2023-03-29 23:24:33 +0200

    ref param name in GetByName...

--- Project_Main/Models/DataBases/Repositories/IUserRepository.cs
+++ Project_Main/Models/DataBases/Repositories/IUserRepository.cs
@@ -5,7 +5,7 @@ namespace Project_Main.Models.DataBases.Repositories
 	public interface IUserRepository : IGenericRepository<UserModel>
 	{
 		Task<UserModel> GetWithDetailsAsync(string id);
-		Task<UserModel> GetByNameAndPasswordAsync(string name, string password);
+		Task<UserModel?> GetByNameAndPasswordAsync(string login, string password);
 		Task<bool> IsNameTakenAsync(string name);
 	}
 }
