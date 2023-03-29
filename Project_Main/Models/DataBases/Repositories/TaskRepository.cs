f2d00d4343a6a21ac4642712373bd9384cc188a7
= <=>
2023-03-29 23:23:23 +0200

    feat Task Repo

---
+++ Project_Main/Models/DataBases/Repositories/TaskRepository.cs
@@ -0,0 +1,11 @@
+﻿using Project_DomainEntities;
+
+namespace Project_Main.Models.DataBases.Repositories
+{
+	public class TaskRepository : GenericRepository<TaskModel>, ITaskRepository
+	{
+		public TaskRepository(CustomDbContext dbContext) : base(dbContext)
+		{
+		}
+	}
+}
