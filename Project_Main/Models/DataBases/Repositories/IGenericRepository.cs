eab58f12ed0f232bc3a8447b2b2aac2dbdb166e1
= <=>
2023-03-29 18:47:08 +0200

    feat Generic Repository Interface

---
+++ Project_Main/Models/DataBases/Repositories/IGenericRepository.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq.Expressions;
+
+namespace Project_Main.Models.DataBases.Repositories
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task Add(T entity);
+        Task<T?> GetAsync(object id);
+        Task<IEnumerable<T>> GetByFilterAsync(Expression<Func<T, bool>>? filter = null);
+		void Update(T entity);
+		void Remove(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+    }
+}
