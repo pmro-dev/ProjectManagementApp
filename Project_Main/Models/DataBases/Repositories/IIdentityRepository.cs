fc007a3042ef240835aa7a3a66b8d77580c9e10f
= <=>
2023-03-29 23:25:21 +0200

    feat Identity Repo Interface

--- Project_Main/Models/DataBases/Repositories/IIdentityRepository.cs
+++ Project_Main/Models/DataBases/Repositories/IIdentityRepository.cs
@@ -4,12 +4,8 @@ namespace Project_Main.Models.DataBases.Repositories
 {
 	public interface IIdentityRepository : IGenericRepository<UserModel>
 	{
-		public Task<bool> AddUserAsync(UserModel newUser);
-		public Task<UserModel> GetUserWithDetailsAsync(string userId);
-		public Task<UserModel> GetUserAsync(string userId);
-		public Task<UserModel> GetUserForLoggingAsync(string userName, string userPassword);
-		public Task<bool> UpdateUserAsync(UserModel userToUpdate);
-		public Task<bool> DeleteUserAsync(string userId);
-		public Task<bool> IsUserNameUsedAsync(string userName);
+		public Task<UserModel> GetWithDetailsAsync(string userId);
+		public Task<UserModel> GetForLoggingAsync(string userName, string userPassword);
+		public Task<bool> IsNameAlreadyUsedAsync(string userName);
 	}
 }
