cddf4bd40b1a6809ca01d0510ba43a98ab23eec2
= <=>
2023-03-30 20:26:41 +0200

    feat identity repository interface

--- Project_Main/Models/DataBases/Repositories/IIdentityRepository.cs
+++ Project_Main/Models/DataBases/Repositories/IIdentityRepository.cs
@@ -4,8 +4,8 @@ namespace Project_Main.Models.DataBases.Repositories
 {
 	public interface IIdentityRepository : IGenericRepository<UserModel>
 	{
-		public Task<UserModel> GetWithDetailsAsync(string userId);
-		public Task<UserModel> GetForLoggingAsync(string userName, string userPassword);
-		public Task<bool> IsNameAlreadyUsedAsync(string userName);
+		public Task<UserModel?> GetWithDetailsAsync(string userId);
+		public Task<UserModel?> GetForLoggingAsync(string userName, string userPassword);
+		public Task<bool> IsNameTakenAsync(string userName);
 	}
 }
