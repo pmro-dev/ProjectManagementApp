36118a260b969dd85cb020c5b8be7854d7fd74c8
= <=>
2023-03-30 23:06:06 +0200

    feat Unit of Work layer for data from AppDbContext

---
+++ Project_Main/Models/DataBases/Repositories/DataUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿namespace Project_Main.Models.DataBases.Repositories
+{
+	public class DataUnitOfWork : UnitOfWork<CustomAppDbContext>, IDataUnitOfWork
+	{
+		public ITodoListRepository TodoListRepository { get; }
+
+		public ITaskRepository TaskRepository { get; }
+
+		public DataUnitOfWork(CustomAppDbContext context, ITodoListRepository todoListRepository, ITaskRepository taskRepository)
+			: base(context)
+		{
+			TodoListRepository = todoListRepository;
+			TaskRepository = taskRepository;
+		}
+	}
+}
---
+++ Project_Main/Models/DataBases/Repositories/IDataUnitOfWork.cs
@@ -0,0 +1,8 @@
+﻿namespace Project_Main.Models.DataBases.Repositories
+{
+	public interface IDataUnitOfWork : IUnitOfWork
+	{
+		ITodoListRepository TodoListRepository { get; }
+		ITaskRepository TaskRepository { get; }
+	}
+}
