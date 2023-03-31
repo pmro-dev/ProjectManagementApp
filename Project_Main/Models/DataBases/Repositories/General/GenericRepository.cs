a7f5c5a57a0e99f60dfd00e958a350cf6dae9848
= <=>
2023-03-31 21:47:51 +0200

    feat new methods first allow to check that a certain DbSet contains any entities and second allows add entities as range

---
+++ Project_Main/Models/DataBases/Repositories/General/GenericRepository.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Project_Main.Infrastructure.Helpers;
+using Project_Main.Models.DataBases.Repositories.Identity;
+using System.Linq.Expressions;
+
+namespace Project_Main.Models.DataBases.Repositories.General
+{
+    public class GenericRepository<TEntity> : IGenericRepository<TEntity> where TEntity : class
+    {
+        protected readonly DbContext Context;
+        private readonly DbSet<TEntity> _dbSet;
+        private readonly ILogger<GenericRepository<TEntity>> _logger;
+        private string operationName = string.Empty;
+
+        public GenericRepository(DbContext dbContext, ILogger<GenericRepository<TEntity>> logger)
+        {
+            Context = dbContext;
+            _dbSet = Context.Set<TEntity>();
+            _logger = logger;
+        }
+
+        public async Task AddAsync(TEntity entity)
+        {
+            operationName = HelperOther.CreateActionNameForLoggingAndExceptions(nameof(AddAsync), nameof(UserRepository));
+            HelperCheck.IfArgumentModelNullThrowException(operationName, entity, nameof(entity), _logger);
+            await _dbSet.AddAsync(entity);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetAllAsync()
+        {
+            return await _dbSet.ToListAsync();
+        }
+
+        public async Task<TEntity?> GetAsync(object id)
+        {
+            operationName = HelperOther.CreateActionNameForLoggingAndExceptions(nameof(GetAsync), nameof(UserRepository));
+            HelperCheck.IfArgumentModelNullThrowException(operationName, id, nameof(id), _logger);
+
+            return await _dbSet.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<TEntity>> GetByFilterAsync(Expression<Func<TEntity, bool>>? filter = null)
+        {
+            IQueryable<TEntity> entities = _dbSet;
+
+            if (filter != null)
+            {
+                entities = entities.Where(filter);
+            }
+
+            return await entities.ToListAsync();
+        }
+
+        public void Remove(TEntity entity)
+        {
+            operationName = HelperOther.CreateActionNameForLoggingAndExceptions(nameof(Remove), nameof(UserRepository));
+            HelperCheck.IfArgumentModelNullThrowException(operationName, entity, nameof(entity), _logger);
+
+            _dbSet.Remove(entity);
+        }
+
+        public void Update(TEntity entity)
+        {
+            operationName = HelperOther.CreateActionNameForLoggingAndExceptions(nameof(Update), nameof(UserRepository));
+            HelperCheck.IfArgumentModelNullThrowException(operationName, entity, nameof(entity), _logger);
+
+            _dbSet.Update(entity);
+        }
+
+        public async Task AddRangeAsync(IEnumerable<TEntity> range)
+        {
+            await _dbSet.AddRangeAsync(range);
+        }
+
+        public async Task<bool> ContainsAny()
+        {
+            var cos = await _dbSet.CountAsync();
+
+            var cos2 = cos + 1;
+
+            return await _dbSet.AnyAsync();
+		}
+    }
+}
---
+++ Project_Main/Models/DataBases/Repositories/General/IGenericRepository.cs
@@ -0,0 +1,16 @@
+﻿using System.Linq.Expressions;
+
+namespace Project_Main.Models.DataBases.Repositories.General
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task AddAsync(T entity);
+        Task<T?> GetAsync(object id);
+        Task<IEnumerable<T>> GetByFilterAsync(Expression<Func<T, bool>>? filter = null);
+        void Update(T entity);
+        void Remove(T entity);
+        Task<IEnumerable<T>> GetAllAsync();
+		Task<bool> ContainsAny();
+		Task AddRangeAsync(IEnumerable<T> range);
+	}
+}
