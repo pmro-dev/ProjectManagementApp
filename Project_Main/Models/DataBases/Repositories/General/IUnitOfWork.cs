4bebcefe1a5b7b2c7dcd950c53509d37aa245c57
= <=>
2023-03-31 21:49:43 +0200

    feat add new methods that allow to use transaction and migrate pending migrations

---
+++ Project_Main/Models/DataBases/Repositories/General/IUnitOfWork.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Project_Main.Models.DataBases.Repositories.General
+{
+    //public interface IUnitOfWork<TContext> : IDisposable where TContext : DbContext
+    //{
+    //	ITodoListRepository TodoLists { get; }
+    //	ITaskRepository Tasks { get; }
+    //	Task SaveChangesAsync();
+    //}
+
+    public interface IUnitOfWork : IDisposable
+    {
+        Task SaveChangesAsync();
+
+        public Task BeginTransactionAsync();
+
+        public Task CommitTransactionAsync();
+
+        public Task RollbackTransactionAsync();
+
+        public Task<IEnumerable<string>> GetPendingMigrationsAsync();
+
+        public Task MigrateAsync();
+	}
+}
