5eb4277cbd1aeb8db01418c47026e7c4764f3fa1
= <=>
2023-04-01 22:50:15 +0200

    fix set table names for new DbContextes to be able work with already created Databases

--- Project_Main/Models/DataBases/Repositories/Identity/CustomIdentityDbContext.cs
+++ Project_Main/Models/DataBases/Repositories/Identity/CustomIdentityDbContext.cs
@@ -16,7 +16,15 @@ namespace Project_Main.Models.DataBases.Repositories.Identity
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             base.OnModelCreating(modelBuilder);
-            modelBuilder.Entity<UserRoleModel>().HasKey(ur => new { ur.UserId, ur.RoleId });
+
+			modelBuilder.Entity<RoleModel>()
+				.ToTable("Roles");
+			modelBuilder.Entity<UserModel>()
+				.ToTable("Users");
+			modelBuilder.Entity<UserRoleModel>()
+				.ToTable("UserRoles");
+
+			modelBuilder.Entity<UserRoleModel>().HasKey(ur => new { ur.UserId, ur.RoleId });
 
             _logger?.LogInformation(Messages.BuildingSucceedLogger, nameof(OnModelCreating), nameof(CustomIdentityDbContext));
         }
