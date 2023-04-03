c5e60abda15ca729cb394611cdddf81226515151
= <=>
2023-04-03 15:30:52 +0200

    feat make Update and Remove CRUD method to run as a Task

--- Project_Main/Models/DataBases/Repositories/General/IGenericRepository.cs
+++ Project_Main/Models/DataBases/Repositories/General/IGenericRepository.cs
@@ -7,8 +7,8 @@ namespace Project_Main.Models.DataBases.Repositories.General
         Task AddAsync(T entity);
         Task<T?> GetAsync(object id);
         Task<IEnumerable<T>> GetByFilterAsync(Expression<Func<T, bool>>? filter = null);
-        void Update(T entity);
-        void Remove(T entity);
+        Task Update(T entity);
+        Task Remove(T entity);
         Task<IEnumerable<T>> GetAllAsync();
 		Task<bool> ContainsAny();
 		Task AddRangeAsync(IEnumerable<T> range);
