6712c0e523d16c224e5f6e7a2fe5ef91b7582372
PMRO <markowskipriv@gmail.com>
2023-10-17 21:13:36 +0200

    fix add more required properties to view model for task creation

--- Project_Main/Models/ViewModels/OutputModels/TaskCreateOutputVM.cs
+++ Project_Main/Models/ViewModels/OutputModels/TaskCreateOutputVM.cs
@@ -5,5 +5,11 @@
 		public int TodoListId { get; set; }
 
 		public string UserId { get; set; } = string.Empty;
+
+		public string TodoListName { get; set; } = string.Empty;
+		public string Title { get; set; } = string.Empty;
+		public string Description { get; set; } = string.Empty;
+		public DateTime DueDate { get; set; }
+		public DateTime ReminderDate { get; set; }
 	}
 }
