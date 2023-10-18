a6f9e086b5e7fcae3700621a35a8b7e04574e96f
PMRO <markowskipriv@gmail.com>
2023-10-18 19:43:35 +0200

    feat const structs that contain controllers, actions and views names.

--- Project_Main/Infrastructure/Helpers/HelperProgram.cs
+++ Project_Main/Infrastructure/Helpers/HelperProgram.cs
@@ -28,4 +28,99 @@ namespace Project_Main.Infrastructure.Helpers
 
 		#endregion
 	}
+
+
+	#region Controllers Constants
+
+	public readonly struct AccountCtrl
+	{
+		public const string Name = "Account";
+		public const string LoginByProviderAction = "LoginByProvider";
+		public const string RegisterAction = "Register";
+		public const string LoginAction = "Login";
+		public const string ErrorAction = "Error";
+		public const string GoogleProvider = "google";
+	}
+
+	public readonly struct BoardsCtrl
+	{
+		public const string Name = "Boards";
+		public const string BrieflyAction = "Briefly";
+		public const string AllAction = "All";
+		public const string SingleDetailsAction = "SingleDetails";
+	}
+
+	public readonly struct TaskCtrl
+	{
+		public const string Name = "Task";
+		public const string DetailsAction = "Details";
+		public const string CreateAction = "Create";
+		public const string EditAction = "Edit";
+		public const string DeleteAction = "Delete";
+		public const string DeleteConfirmedAction = "DeleteConfirmed";
+	}
+
+	public readonly struct TodoListCtrl
+	{
+		public const string Name = "TodoList";
+		public const string CreateAction = "Create";
+		public const string EditAction = "Edit";
+		public const string DeleteAction = "Delete";
+		public const string DeleteConfirmedAction = "DeleteConfirmed";
+		public const string DuplicateAction = "Duplicate";
+	}
+
+	#endregion
+
+
+	#region Views Constants
+
+	public readonly struct TaskViews
+	{
+		public const string FolderName = "Task";
+		public const string Create = "CreateTask";
+		public const string Delete = "DeleteTask";
+		public const string Details = "DetailsTask";
+		public const string Edit = "EditTask";
+	}
+
+	public readonly struct TodoListViews
+	{
+		public const string FolderName = "TodoList";
+		public const string Create = "CreateTodoList";
+		public const string Delete = "DeleteTodoList";
+		public const string Edit = "EditTodoList";
+	}
+
+	public readonly struct BoardsViews
+	{
+		public const string FolderName = "Boards";
+		public const string All = "All";
+		public const string Briefly = "Briefly";
+		public const string SingleDetails = "SingleDetails";
+		public const string DeleteModal = "_DeleteModal";
+	}
+
+	public readonly struct AccountViews
+	{
+		public const string FolderName = "Account";
+		public const string Login = "Login";
+		public const string Register = "Register";
+		public const string DefaultLayout = "_LoginLayout";
+	}
+
+	public readonly struct SharedViews
+	{
+		public const string FolderName = "Shared";
+		public const string DefaultLayout = "_Layout";
+		public const string TaskTable = "_TaskTableDetails";
+		public const string TodoListTable = "_TodoListTable";
+		public const string TodoListTableDetails = "_TodoListTableDetails";
+		public const string Validation = "_ValidationScriptsPartial";
+		public const string AccessDenied = "AccessDenied";
+		public const string Error = "Error";
+	}
+
+	#endregion
+
 }
