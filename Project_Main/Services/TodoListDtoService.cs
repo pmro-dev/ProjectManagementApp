f0117db2f33cf68e4cda244cea539a7fccfdcad9
PMRO <markowskipriv@gmail.com>
2023-10-19 22:24:05 +0200

    feat create new methods for transfering from Dto model to default model and from Dto model to

--- Project_Main/Services/TodoListDtoService.cs
+++ Project_Main/Services/TodoListDtoService.cs
@@ -1,5 +1,6 @@
 ﻿using Project_DomainEntities;
 using Project_Main.Infrastructure.DTOs;
+using Project_Main.Models.ViewModels.OutputModels;
 
 namespace Project_Main.Services
 {
@@ -13,6 +14,30 @@ namespace Project_Main.Services
                 Title = todoListModel.Title,
                 UserId = todoListModel.UserId,
                 Tasks = todoListModel.Tasks
+            };
+        }
+
+		public static BoardsAllOutputVM TransferToBoardsAllOutputVM(TodoListModelDto todoListDto)
+		{
+			return new BoardsAllOutputVM
+			{
+				Id = todoListDto.Id,
+				Title = todoListDto.Title,
+				UserId = todoListDto.UserId,
+				Tasks = todoListDto.Tasks.Select(t => t).ToList()
+			};
+		}
+
+        public static BoardsSingleDetailsOutputVM TransferToSingleDetailsOutputVM(TodoListModelDto todoListDto, DateTime? filterDueDate = null)
+        {
+			return new BoardsSingleDetailsOutputVM
+			{
+				Id = todoListDto.Id,
+				Name = todoListDto.Title,
+				TasksForToday = TasksFilterService.FilterForTasksForToday(todoListDto.Tasks),
+				TasksCompleted = TasksFilterService.FilterForTasksCompleted(todoListDto.Tasks),
+				TasksNotCompleted = TasksFilterService.FilterForTasksNotCompleted(todoListDto.Tasks, filterDueDate),
+				TasksExpired = TasksFilterService.FilterForTasksExpired(todoListDto.Tasks)
             };
         }
     }
