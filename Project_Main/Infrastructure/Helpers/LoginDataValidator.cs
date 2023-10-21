039854e6eb3d33124299d8ec77a80e9d68a4462f
PMRO <markowskipriv@gmail.com>
2023-10-21 21:36:38 +0200

    feat create validator class for login data - used in Account controller

---
+++ Project_Main/Infrastructure/Helpers/LoginDataValidator.cs
@@ -0,0 +1,12 @@
+﻿using Project_Main.Infrastructure.DTOs;
+
+namespace Project_Main.Infrastructure.Helpers
+{
+	public static class LoginDataValidator
+	{
+		public static bool Valid(LoginInputDto loginInputDto)
+		{
+			return string.IsNullOrEmpty(loginInputDto.Username) || string.IsNullOrEmpty(loginInputDto.Password);
+		}
+	}
+}
