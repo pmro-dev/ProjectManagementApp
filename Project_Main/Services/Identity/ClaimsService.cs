ec4384e8a90be25f484108295b092c4fd53e66c3
PMRO <markowskipriv@gmail.com>
2023-10-21 21:48:21 +0200

    ref start to use Dto transformation within ClaimsService and move whole class to new destination /Services/Identity

---
+++ Project_Main/Services/Identity/ClaimsService.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Project_IdentityDomainEntities;
+using Project_Main.Infrastructure.DTOs;
+using System.Security.Claims;
+
+namespace Project_Main.Services.Identity
+{
+    public class ClaimsService : IClaimsService
+    {
+        public ClaimsPrincipal CreateUserClaimsPrincipal(UserDto userDto)
+        {
+            var userClaims = CreateUserClaims(userDto);
+
+            ClaimsIdentity userClaimsIdentity = new(userClaims, CookieAuthenticationDefaults.AuthenticationScheme);
+            ClaimsPrincipal userClaimsPrincipal = new(userClaimsIdentity);
+
+            return userClaimsPrincipal;
+        }
+
+		#region Local Methods
+
+		private static List<Claim> CreateUserClaims(UserDto userDto)
+        {
+            var userClaims = new List<Claim>
+                    {
+                        new Claim(ClaimTypes.Name, userDto.Username),
+                        new Claim(ClaimTypes.Email, userDto.Email),
+                        new Claim(ClaimTypes.NameIdentifier, userDto.NameIdentifier),
+                        new Claim(ClaimTypes.Surname, userDto.LastName),
+                        new Claim(ClaimTypes.GivenName, userDto.FirstName),
+                    };
+
+            AddRolesToUserClaims(userClaims, userDto.UserRoles);
+
+            return userClaims;
+        }
+
+        private static void AddRolesToUserClaims(List<Claim> userClaims, List<UserRoleModel> newClaims)
+        {
+            foreach (var userRole in newClaims)
+            {
+                userClaims.Add(new Claim(ClaimTypes.Role, userRole.Role.Name));
+            }
+        }
+
+		#endregion
+	}
+}
---
+++ Project_Main/Services/Identity/IClaimsService.cs
@@ -0,0 +1,10 @@
+﻿using Project_Main.Infrastructure.DTOs;
+using System.Security.Claims;
+
+namespace Project_Main.Services.Identity
+{
+    public interface IClaimsService
+    {
+        public ClaimsPrincipal CreateUserClaimsPrincipal(UserDto userDto);
+    }
+}
