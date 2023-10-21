37c7dde1578bf9740831871f4c5dd643895ce8ba
PMRO <markowskipriv@gmail.com>
2023-10-21 21:17:04 +0200

    ref move UserAuthenticationService to /Services/Identity directory and add method to authenticate user (old location was UserService)

---
+++ Project_Main/Services/Identity/IUserAuthenticationService.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace Project_Main.Services.Identity
+{
+    public interface IUserAuthenticationService
+    {
+        ChallengeResult ChallengeProviderToLogin(string provider);
+
+        AuthenticationProperties CreateDefaultAuthProperties();
+
+        public bool AuthenticateUser(ClaimsPrincipal userPrincipal);
+	}
+}
