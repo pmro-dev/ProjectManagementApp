f2b42d8a75d019fd493711f6ff39e65fee9c5c8c
PMRO <markowskipriv@gmail.com>
2023-10-21 21:31:51 +0200

    ref delete requirement of passing params to LogoutByProvider method within LogoutService, signout user within this method and start to return specific IActionResult based on User Auth Scheme

--- Project_Main/Services/ILogoutService.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Project_Main.Services
-{
-	public interface ILogoutService
-	{
-		Task<ActionResult> LogoutByProviderTypeAsync(ControllerBase controller, string userAuthenticationScheme);
-	}
-}
---
+++ Project_Main/Services/Identity/ILogoutService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Project_Main.Services.Identity
+{
+    public interface ILogoutService
+    {
+        Task<IActionResult> LogoutByProviderAsync();
+    }
+}
