568040dafefbf60b2ccb353d8c3450ecdaf0ecee
PMRO <markowskipriv@gmail.com>
2023-10-21 21:37:58 +0200

    fix add slash to custom routes and update MainBoard Route and create const for Main Board Uri

--- Project_Main/Infrastructure/Helpers/CustomRoutes.cs
+++ Project_Main/Infrastructure/Helpers/CustomRoutes.cs
@@ -11,18 +11,18 @@
         public const string TodoListControllerRoute = TodoListCtrl.Name;
         public const string AccountControllerRoute = AccountCtrl.Name;
 
-		public const string MainBoardRouteName = "MainBoard";
-        public const string MainBoardRoute = "[controller]/[action]";
+        public const string MainBoardRoute = "/[controller]/[action]";
+        public const string MainBoardUri = BoardsCtrl.Name + "/" + BoardsCtrl.BrieflyAction;
 
-        public const string AllDetailsRoute = "[controller]/[action]";
-        public const string TodoListDetailsRoute = "Boards/Details/[controller]/{id:int}";
-        public const string TodoListCreateRoute = "[controller]/[action]";
-		public const string TodoListEditRoute = "[controller]/{id:int}/[action]";
-        public const string TodoListDeleteRoute = "[controller]/{id:int}/[action]";
-        public const string TodoListDeletePostRoute = "[controller]/{id:int}/[action]";
-		public const string TodoListDuplicateRoute = "[controller]/{todoListId:int}/[action]";
-        public const string LoginByProviderRoute = "[action]/{provider}";
-        public const string TaskDetailsRoute = $"Boards/[action]/{TodoListCtrl.Name}/{{routeTodoListId:int}}/[controller]/{{routeTaskId:int}}";
+		public const string AllDetailsRoute = "/[controller]/[action]";
+        public const string TodoListDetailsRoute = "/Boards/Details/[controller]/{id:int}";
+        public const string TodoListCreateRoute = "/[controller]/[action]";
+		public const string TodoListEditRoute = "/[controller]/{id:int}/[action]";
+        public const string TodoListDeleteRoute = "/[controller]/{id:int}/[action]";
+        public const string TodoListDeletePostRoute = "/[controller]/{id:int}/[action]";
+		public const string TodoListDuplicateRoute = "/[controller]/{todoListId:int}/[action]";
+        public const string LoginByProviderRoute = "/[action]/{provider}";
+        public const string TaskDetailsRoute = $"/Boards/[action]/{TodoListCtrl.Name}/{{routeTodoListId:int}}/[controller]/{{routeTaskId:int}}";
         public const string CreateTaskRoute = TodoListCtrl.Name + "/{id:int}/[controller]/[action]";
         public const string CreateTaskPostRoute = TodoListCtrl.Name + "/{todoListId:int}/[controller]/[action]";
         public const string TaskEditRoute = TodoListCtrl.Name + "/{todoListId:int}/[controller]/{taskId:int}/[action]";
