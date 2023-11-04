9f44387e22a44082e16a73b7fcc4a4bc8044a00b
PMRO <markowskipriv@gmail.com>
2023-11-04 21:12:30 +0100

    feat wrapper view model

---
+++ Project_Main/Models/ViewModels/WrapperModels/IWrapperViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Project_Main.Models.ViewModels.WrapperModels
+{
+    public interface IWrapperViewModel<InputViewModel, OutputViewModel> where InputViewModel : class, new() where OutputViewModel : class, new()
+    {
+        InputViewModel InputVM { get; set; }
+        OutputViewModel OutputVM { get; set; }
+    }
+}
---
+++ Project_Main/Models/ViewModels/WrapperModels/WrapperViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace Project_Main.Models.ViewModels.WrapperModels
+{
+    public class WrapperViewModel<InputViewModel, OutputViewModel> : IWrapperViewModel<InputViewModel, OutputViewModel> where InputViewModel : class, new() where OutputViewModel : class, new()
+    {
+        public InputViewModel InputVM { get; set; }
+        public OutputViewModel OutputVM { get; set; }
+
+        public WrapperViewModel()
+        {
+            InputVM = new InputViewModel();
+            OutputVM = new OutputViewModel();
+        }
+
+        public WrapperViewModel(InputViewModel inputVM, OutputViewModel outputVM)
+        {
+            InputVM = inputVM;
+            OutputVM = outputVM;
+        }
+    }
+}
