7177e5ac3eac82ba05d13d9f079be036431e701d
PMRO <markowskipriv@gmail.com>
2023-11-07 16:00:50 +0100

    fix views usings after folders architecture manipulation and create interface that allows to use different view models within shared partial view of task table details

--- Project_Main/Models/Outputs/ViewModels/ITaskDetailsOutputVM.cs
+++ Project_Main/Models/Outputs/ViewModels/ITaskDetailsOutputVM.cs
@@ -1,18 +1,9 @@
 ﻿using Project_DomainEntities.Helpers;
+using System.ComponentModel.DataAnnotations;
 
 namespace Project_Main.Models.Outputs.ViewModels
 {
-    public interface ITaskDetailsOutputVM
+    public interface ITaskDetailsOutputVM : ITaskCommonOutputVM
     {
-        int Id { get; set; }
-        string Title { get; set; }
-        string Description { get; set; }
-        DateTime CreationDate { get; set; }
-        DateTime DueDate { get; set; }
-        DateTime LastModificationDate { get; set; }
-        DateTime? ReminderDate { get; set; }
-        TaskStatusHelper.TaskStatusType Status { get; set; }
-        int TodoListId { get; set; }
-        string UserId { get; set; }
     }
 }
