100238f210d41125a1bb550fcead4c9ce808802c
PMRO <markowskipriv@gmail.com>
2023-11-18 20:51:10 +0100

    delete unused files

--- App/Features/Tasks/Create/TaskCreationOutputDto.cs
+++
@@ -1,9 +0,0 @@
-﻿//namespace Project_Main.Models.Outputs.DTOs
-//{
-//    public class TaskCreationOutputDto : ITaskCreateOutputDto
-//    {
-//        public int TodoListId { get; set; }
-//        public string UserId { get; set; } = string.Empty;
-//        public string TodoListName { get; set; } = string.Empty;
-//    }
-//}
