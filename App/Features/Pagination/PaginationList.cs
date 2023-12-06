824d7037982888bc9abc9544387d8183adf59f94
PMRO <markowskipriv@gmail.com>
2023-12-06 10:40:41 +0100

    feat create pagination list model

---
+++ App/Features/Pagination/PaginationList.cs
@@ -0,0 +1,13 @@
+﻿namespace App.Features.Pagination;
+
+public class PaginationList<T> : List<T> where T : class
+{
+	public List<T> Items { get; set; }
+
+    public PaginationList(IQueryable<T> items, int pageNumber, int itemsPerPageCount)
+    {
+		int itemsCountToSkip = pageNumber * itemsPerPageCount;
+
+		Items = items.Skip(itemsCountToSkip).Take(itemsPerPageCount).ToList();
+	}
+}
