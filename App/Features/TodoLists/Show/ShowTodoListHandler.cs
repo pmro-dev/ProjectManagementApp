1ece131dd16a0c4a29b54f3bbfc40dfa0a4d484e
PMRO <markowskipriv@gmail.com>
2023-12-14 22:33:17 +0100

    fix bug with wrong tasks count value that is provided to Show TodooList handler, read tasks count separately from Db

--- App/Features/TodoLists/Show/ShowTodoListHandler.cs
+++ App/Features/TodoLists/Show/ShowTodoListHandler.cs
@@ -11,31 +11,33 @@ public class ShowTodoListHandler : IRequestHandler<ShowTodoListQuery, ShowTodoLi
 {
 	private readonly IDataUnitOfWork _dataUnitOfWork;
 	private readonly ITodoListRepository _todoListRepository;
+	private readonly ITaskRepository _taskRepository;
 	private readonly ILogger<ShowTodoListHandler> _logger;
 	private readonly ITodoListMapper _todoListMapper;
 	private readonly ITodoListViewModelsFactory _todoListViewModelsFactory;
 
-	public ShowTodoListHandler(IDataUnitOfWork dataUnitOfWork, ILogger<ShowTodoListHandler> logger, 
-		ITodoListMapper todoListMapper, ITodoListViewModelsFactory todoListViewModelsFactory)
+	public ShowTodoListHandler(IDataUnitOfWork dataUnitOfWork, ILogger<ShowTodoListHandler> logger,
+		ITodoListMapper todoListMapper, ITodoListViewModelsFactory todoListViewModelsFactory, ITaskRepository taskRepository)
 	{
 		_logger = logger;
 		_dataUnitOfWork = dataUnitOfWork;
 		_todoListRepository = _dataUnitOfWork.TodoListRepository;
 		_todoListMapper = todoListMapper;
 		_todoListViewModelsFactory = todoListViewModelsFactory;
+		_taskRepository = taskRepository;
 	}
 
 	public async Task<ShowTodoListQueryResponse> Handle(ShowTodoListQuery request, CancellationToken cancellationToken)
 	{
 		ExceptionsService.WhenValueLowerThanBottomBoundryThrow(nameof(ShowTodoListQuery), request.TodoListId, nameof(request.TodoListId), _logger);
 
-		TodoListModel? todoListDbModel = await _todoListRepository.GetSingleWithDetailsAsync(request.TodoListId, request.OrderDetailsBySelector, request.PageNumber, request.ItemsPerPageCount);
+		int tasksCount = await _taskRepository.CountAsync(task => task.TodoListId == request.TodoListId);
 		PaginationData paginData = new(request.PageNumber, request.ItemsPerPageCount, tasksCount, _logger);
-		ExceptionsService.WhenEntityIsNullThrow(nameof(ShowTodoListQuery), todoListDbModel, _logger, request.TodoListId);
-		var todoListDto = _todoListMapper.TransferToDto(todoListDbModel!);
 
-		PaginationData paginData = new(request.PageNumber, request.ItemsPerPageCount, todoListDto.Tasks.Count, _logger);
+		TodoListModel? todoList = await _todoListRepository.GetSingleWithDetailsAsync(request.TodoListId, request.OrderDetailsBySelector, paginData.CurrentPageNumber, paginData.ItemsPerPageCount);
+		ExceptionsService.WhenEntityIsNullThrow(nameof(ShowTodoListQuery), todoList, _logger, request.TodoListId);
 
+		var todoListDto = _todoListMapper.TransferToDto(todoList!);
 		var data = _todoListViewModelsFactory.CreateDetailsOutputVM(todoListDto, paginData, request.FilterDueDate);
 
 		return new ShowTodoListQueryResponse(data);
