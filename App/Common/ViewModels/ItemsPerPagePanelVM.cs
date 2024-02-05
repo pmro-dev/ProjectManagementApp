96f1263e2d57fbd0fc950ee51e943ed1cf4ccb1a
PMRO <markowskipriv@gmail.com>
2024-02-05 15:23:36 +0100

    fix bug with binding filter due date to items per page

---
+++ App/Common/ViewModels/ItemsPerPagePanelVM.cs
@@ -0,0 +1,13 @@
+﻿namespace App.Common.ViewModels;
+
+public class ItemsPerPagePanelVM
+{
+	public int TodoListId { get; init; }
+	public DateTime? FilterDueDate { get; init; }
+
+	public ItemsPerPagePanelVM(int todoListId, DateTime? filterDueDate)
+	{
+		TodoListId = todoListId;
+		FilterDueDate = filterDueDate;
+	}
+}
