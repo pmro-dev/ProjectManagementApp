85f759d1078a821aaee3b2264acd43c0887b05e4
PMRO <markowskipriv@gmail.com>
2024-02-23 17:40:10 +0100

    feat add commands and queries for project entity creation

---
+++ App/Features/Projects/Create/CreateProjectCommand.cs
@@ -0,0 +1,19 @@
+﻿using App.Features.Projects.Create.Models;
+using MediatR;
+
+namespace App.Features.Projects.Create;
+
+public class CreateProjectCommand : IRequest<CreateProjectCommandResponse>
+{
+	public ProjectCreateInputVM InputVM { get; }
+
+	public CreateProjectCommand(ProjectCreateInputVM inputVM)
+	{
+		InputVM = inputVM;
+	}
+}
+
+public record CreateProjectCommandResponse(
+	string? ErrorMessage = null,
+	int StatusCode = StatusCodes.Status201Created
+){}
---
+++ App/Features/Projects/Create/CreateProjectQuery.cs
@@ -0,0 +1,16 @@
+﻿using App.Common.ViewModels;
+using App.Features.Projects.Create.Models;
+using MediatR;
+
+namespace App.Features.Projects.Create;
+
+public class CreateProjectQuery : IRequest<CreateProjectQueryResponse>
+{
+}
+
+
+public record CreateProjectQueryResponse(
+	WrapperViewModel<ProjectCreateInputVM, ProjectCreateOutputVM>? Data,
+	string? ErrorMessage = null,
+	int StatusCode = StatusCodes.Status200OK
+){}
